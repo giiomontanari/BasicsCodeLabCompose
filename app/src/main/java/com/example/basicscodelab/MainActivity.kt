accae8381aabf72d4375f4aff6d40aeccec9acc8
Giovanni Montanari <giovannimontanari3@gmail.com>
2023-06-24 13:25:07 -0300

    fix layout

--- app/src/main/java/com/example/basicscodelab/MainActivity.kt
+++ app/src/main/java/com/example/basicscodelab/MainActivity.kt
@@ -23,6 +23,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.basicscodelab.ui.theme.BasicsCodelabTheme
@@ -88,7 +89,12 @@ private fun Greeting(name: String) {
                     .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
                 Text(text = "Hello")
-                Text(text = name)
+                Text(
+                    text = name,
+                    style = MaterialTheme.typography.h3.copy(
+                        fontWeight = FontWeight.ExtraBold
+                    )
+                )
             }
             ElevatedButton(
                 colors = ButtonDefaults.buttonColors(
@@ -101,6 +107,7 @@ private fun Greeting(name: String) {
         }
     }
 }
+
 @Composable
 fun OnboardingScreen(
     onContinueClicked: () -> Unit,
